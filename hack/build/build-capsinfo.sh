1907fcd30a2aa3a33c2bf1113fa6825c32fd0aa3
Francesco Romani <fromani@redhat.com>
2019-04-02 14:19:04 +0200

    hack: headers: fix build/include paths
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- hack/build/build-capsinfo.sh
+++ hack/build/build-capsinfo.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd cmd/kvm-caps-info-nfd-plugin && gcc -Wall -g -I. -DKVMINFO_TOOL=1 -o kvm-caps-info-nfd-plugin kvminfocaps.c
+cd cmd/kvm-caps-info-nfd-plugin && gcc -Wall -g -Ilinux-headers -DKVMINFO_TOOL=1 -o kvm-caps-info-nfd-plugin kvminfocaps.c

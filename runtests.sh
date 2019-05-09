daa29a0266c8271bfaab8bdc156620fabfa64042
Francesco Romani <fromani@redhat.com>
2019-05-09 16:28:24 +0200

    tests: make room for capsinfo tests
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- runtests.sh
+++ runtests.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-if [ ! -x cmd/kvm-info-nfd-plugin/kvm-info-nfd-plugin ]; then
+if [ ! -x cmd/kvm-version-info-nfd-plugin/kvm-version-info-nfd-plugin ]; then
 	echo "*ERR: build the plugin first"
 	exit 1
 fi
 
-for testcase in $( ls -d tests/??? ); do
+for testcase in $( ls -d tests/version-info/??? ); do
 	expected=$(cat $testcase/_stdout)
 	if [ -r $testcase/kvm-version-info.json ]; then
-		result=$(./cmd/kvm-info-nfd-plugin/kvm-info-nfd-plugin $testcase/kvm-version-info.json 2> /dev/null)
+		result=$(./cmd/kvm-version-info-nfd-plugin/kvm-version-info-nfd-plugin $testcase/kvm-version-info.json 2> /dev/null)
 	else
-		result=$(./cmd/kvm-info-nfd-plugin/kvm-info-nfd-plugin 2> /dev/null)
+		result=$(./cmd/kvm-version-info-nfd-plugin/kvm-version-info-nfd-plugin 2> /dev/null)
 	fi
 	if [[ "$result" != "$expected" ]]; then
 		echo "FAIL: $testcase: expected='''$expected''' got='''$result'''"

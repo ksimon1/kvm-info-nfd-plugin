6a971d9e68f98ee2a39a023dd8c30abcdabd918d
Francesco Romani <fromani@redhat.com>
2019-05-29 11:34:33 +0200

    Revert "build: capsinfo as static binary"
    This reverts commit 07760c417439fd7827b70c2da7118a3548151d4d.

--- hack/build/build-capsinfo.sh
+++ hack/build/build-capsinfo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-gcc -static -Wall -g -Ilib -o cmd/kvm-caps-info-nfd-plugin/kvm-caps-info-nfd-plugin \
+gcc -Wall -g -Ilib -o cmd/kvm-caps-info-nfd-plugin/kvm-caps-info-nfd-plugin \
 	cmd/kvm-caps-info-nfd-plugin/main.c \
 	cmd/kvm-caps-info-nfd-plugin/kvm_io.c \
 	lib/libkvminfocaps.a
